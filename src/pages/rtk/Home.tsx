727a67d645b5ea895d97492bb91864f1c39d0777
cnerylozada <cnerylozada@cnerylozadas-MacBook-Pro.local>
2023-10-13 18:02:05 -0500

    feat: get-all-pokemons query was created

--- src/pages/rtk/Home.tsx
+++ src/pages/rtk/Home.tsx
@@ -1,14 +1,21 @@
-import { useGetPokemonByNameQuery } from "../../rtkquery/services/pokemon";
+import { useGetAllPokemonsQuery } from "../../rtkquery/services/pokemon";
 
 export const HomePage = () => {
-  const { data, error, isLoading } = useGetPokemonByNameQuery('bulbasaur')
+  const { data, error, isLoading } = useGetAllPokemonsQuery();
+  // const { data, error, isLoading } = useGetPokemonByNameQuery('bulbasaur')
+
   return (
     <div>
       <div>HomePage</div>
       {isLoading && <div>Loading ...</div>}
       <div>
-        <div>id: {data?.id}</div>
-        <div>name: {data?.name}</div>
+        {data?.pokemons.length && (
+          <div>
+            {data.pokemons.map((_) => (
+              <div key={_}>{_}</div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
--- src/rtkquery/services/pokemon.ts
+++ src/rtkquery/services/pokemon.ts
@@ -1,13 +1,21 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const apiUrl = "https://pokeapi.co/api/v2";
+
 export const pokemonApi = createApi({
   reducerPath: "pokemonApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://pokeapi.co/api/v2/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: `${apiUrl}/` }),
   endpoints: (builder) => ({
-    getPokemonByName: builder.query({
+    getAllPokemons: builder.query<{ pokemons: string[] }, void>({
+      query: () => "pokemon",
+      transformResponse: (response: { results: { name: string }[] }) => {
+        console.log(response);
+        return { pokemons: response.results.map((_) => _.name) };
+      },
+    }),
+    getPokemonByName: builder.query<{ id: number; name: string }, string>({
       query: (name) => `pokemon/${name}`,
       transformResponse: (response: { name: string; id: number }) => {
-        console.log(response)
         return {
           id: response.id,
           name: response.name,
@@ -17,4 +25,4 @@ export const pokemonApi = createApi({
   }),
 });
 
-export const { useGetPokemonByNameQuery } = pokemonApi;
+export const { useGetAllPokemonsQuery, useGetPokemonByNameQuery } = pokemonApi;
